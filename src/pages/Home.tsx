ccb668b9185ce26892313be18cdcaa8f148ba982
KwonEunjjn <kwoneunjin04@naver.com>
2026-04-22 12:49:02 +0900

    07-0421

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,16 @@
+import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
-import styles from "./Home.module.css";
 
+const Wrap = styled.div`
+    padding: 40px; +
+`;
 
-const Wrap = styled.div''
-
-
-function Home() {
-    return (
-        <div className={styles.wrap}>
-            <h2>Google Books 검색</h2>
-            <SearchBar />
-        </div>
-    );
+export default function Home() {
+        return (
+            <Wrap>
+                <h2>Google Books 검색</h2>
+                <SearchBar/>
+            </Wrap>
+        );
 }
 
-export default Home;
